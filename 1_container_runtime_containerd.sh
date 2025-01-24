56248f62ed5c90c624e9cd06b0427a18b35f7a6b
gagaga3849 <78959229+gagaga3849@users.noreply.github.com>
2025-01-24 16:34:59 +0100

    Add files via upload

---
+++ 1_container_runtime_containerd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## IPv4 packet forwarding
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+sysctl net.ipv4.ip_forward
+
+echo "Disabling swap (required for Kubernetes)..."
+sudo swapoff -a
+sudo sed -i '/swap/d' /etc/fstab
+
+#DOCKER
+# Update package index and install prerequisites
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add Docker repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update
+sudo apt install -y containerd.io
+
+
+containerd config default > /etc/containerd/config.toml
+
+echo "update /etc/containerd/config.toml: Systemcgroup=true"
+
+ps -p 1
